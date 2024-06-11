7bf7b18666831298eaa8ed27b4edeeec1b4c7df5
Timothy James <tim.lim@digipen.edu>
2024-06-10 18:02:08 -0700

    Updated Assignment 1
    Modified the initial KNN algo to output Accuracies vs K-count
    
    Added code to find median and inter-quartile range

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -6,6 +6,7 @@ import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 from sklearn import metrics
 from sklearn import svm
 
@@ -42,6 +43,19 @@ ds.drop_duplicates(inplace=True)
 # Question 2
 ds.boxplot()
 plt.savefig('boxplot.jpg', format='jpg')
+
+# used to find inter-quartile range
+mean = ds.drop(['Species'], axis=1)
+median = mean.median()
+Q1 = mean.quantile(0.25)
+Q3 = mean.quantile(0.75)
+print(Q3 - Q1)
+print("\n")
+
+# used to find median
+print(median)
+
+
 # plt.show()
 # 2a) boxplot plots the outliers as circles within the diagram.
 # 2b) The diagram shows 4 outliers within the sepalWidthCm.
@@ -92,11 +106,64 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, train_s
 
 # Euclidean KNN, is this the triangle guy?
 
-knn1 = KNeighborsClassifier(n_neighbors=6, weights='uniform', algorithm='auto', metric='euclidean')
+high_acc = 0.0
+nh = 0
+accuracies1 = []
+for n in range(1,11):
+    knn1 = KNeighborsClassifier(n_neighbors=n, weights='uniform', algorithm='auto', metric='euclidean')
+    knn1.fit(x_train, y_train)
+    x_pre = knn1.predict(x_test)
+
+    accuracy = accuracy_score(y_test, x_pre)
+    accuracies1.append(accuracy)
+    # print(f"Accuracy: {accuracy:.2f}, n_neighbour: {n}")
+    if accuracy > high_acc:
+        high_acc = accuracy
+        nh = n
+
+plt.figure()
+plt.plot(range(1,11), accuracies1, marker='o',  linestyle='-', color='r')
+plt.xlabel('Value of K')
+plt.ylabel('Accuracy')
+plt.title('Accuracy vs Value of K (Euclidean)')
+plt.xticks(range(1,11))
+plt.grid(True)
+plt.savefig('AvK(Euclidean).jpg', format='jpg')
+plt.show()
+
+print(f"highest Accuracy (Euclidean): {high_acc:.2f}, K: {nh}")
 
 # Manhattan's KNN, not a fan of KNN I much more prefer the fish market
 
-knn2 = KNeighborsClassifier(n_neighbors=3, weights='uniform', algorithm='auto', metric='manhattan')
+high_acc = 0.0
+nh = 0
+accuracies2 = []
+for n in range(1,11):
+    knn2 = KNeighborsClassifier(n_neighbors=n, weights='uniform', algorithm='auto', metric='manhattan')
+    knn2.fit(x_train, y_train)
+    x_pre = knn2.predict(x_test)
+
+    accuracy = accuracy_score(y_test, x_pre)
+    accuracies2.append(accuracy)
+    if accuracy > high_acc:
+        high_acc = accuracy
+        nh = n
+    # print(f"Accuracy: {accuracy:.2f}, n_neighbour: {n}")
+    if accuracy > high_acc:
+        high_acc = accuracy
+        nh = n
+
+plt.figure()
+plt.plot(range(1,11), accuracies2, marker='o',  linestyle='-', color='r')
+plt.xlabel('Value of K')
+plt.ylabel('Accuracy')
+plt.title('Accuracy vs Value of K (Manhattan)')
+plt.xticks(range(1,11))
+plt.grid(True)
+plt.savefig('AvK(Manhattan).jpg', format='jpg')
+plt.show()
+
+print(f"highest Accuracy (Manhattan): {high_acc:.2f}, K: {nh}")
 
 # Okay N-Fold Validation here we go!
 # I'm gonna play around with just the training data so using 80/20
@@ -125,10 +192,11 @@ plt.figure()
 plt.plot(n_range, mean_acc, marker='o',  linestyle='-', color='r')
 plt.xlabel('Number of Neighbors')
 plt.ylabel('Accuracy')
-plt.title('Accuracy vs Number of Neighbors')
+plt.title('Accuracy vs Number of Neighbors (N-Fold)')
 plt.xticks(n_range)
 plt.grid(True)
-#plt.show()
+plt.savefig('AvN(N-Fold).jpg', format='jpg')
+plt.show()
 
 # Re-Evaluating this fella!
 knn1 = KNeighborsClassifier(n_neighbors=bn, weights='uniform', algorithm='auto', metric='euclidean')
